aaca71602f872b64249bb115520f4602a30bb591
lbair39 <lilybair24@g.crookcountyschools.org>
2021-10-19 09:51:55 -0700

    I created a JS, a CSS, and a HTML. I also worked on my JS a little.

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('My script is working')
+
+document.write('<h1>This is my JavaScript Demo</h1>')
+
+document.write('<h3>This was created in JS not HTML</h3>')
+
+alert('This entire page was created by using only JS')
+
+prompt('do you like alerts?')
